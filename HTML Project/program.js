d5161eb06e4b18e36d4521f4cf197db552c101db
kvelraj_concord <kvelraj@concord.net>
2021-09-07 08:45:51 +0530

    js

---
+++ HTML Project/program.js
@@ -0,0 +1,16 @@
+// TO create Temporaty storage ( Variable )
+
+// Syntax -> var variable_name = value;
+// variable_name -> Any reasonable text value, a-z, A-Z, 0-9, $, _
+
+// Number -> Any numerical value from Positive, Negative, Decimal, Fractional
+var user_age = 24;
+console.log(user_age);
+
+//string -> Any value which is typed inside dobule quotes " " or single quotes ' '
+var user_name = "karthick kumar";
+console.log(user_name);
+
+// Boolean -> true or false
+var result = false;
+console.log(result)
