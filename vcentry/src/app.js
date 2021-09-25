422c4e7727f9217bfb601e99eaf9a96aba4c4493
kvelraj_concord <kvelraj@concord.net>
2021-09-25 08:43:54 +0530

    react

---
+++ vcentry/src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+
+  render(){
+    return(
+      <div>
+        <h1>Hello To React JS... Welcome</h1>
+        <h1>This is a App.js Component</h1>
+      </div>
+    )
+  }
+
+}
+
+export default App;
