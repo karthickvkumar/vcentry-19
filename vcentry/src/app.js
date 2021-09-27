64bdd83dbb18d46d4250e3aae155ada9ace4bed2
kvelraj_concord <kvelraj@concord.net>
2021-09-27 08:50:33 +0530

    function

--- vcentry/src/app.js
+++ vcentry/src/app.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 class App extends Component{
 
+  display(){
+    let value = prompt("Enter a value...")
+    console.log(value);
+  }
+
   render(){
     return(
       <div>
-        <h1>Hello To React JS... Welcome</h1>
-        <h1>This is a App.js Component</h1>
+        <h1 className="heading">Hello To React JS... Welcome</h1>
+        <h1 className="sub-heading">This is a App.js Component</h1>
+        <button onClick={() => this.display() }>Click this Button</button>
       </div>
     )
   }
--- vcentry/src/index.js
+++ vcentry/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './app';
+import "./css/index.css";
 
 ReactDOM.render(<App></App>, document.getElementById('root'));
 
