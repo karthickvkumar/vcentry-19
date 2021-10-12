58c24ff66d525819b2ddce64fd372571659593f1
kvelraj_concord <kvelraj@concord.net>
2021-10-12 08:40:53 +0530

    sumbmit user

--- vcentry/src/pages/inbox.js
+++ vcentry/src/pages/inbox.js
@@ -1,11 +1,41 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class InboxPage extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name : '',
+      job : ''
+    }
+  }
+
+  onHandleInput = (event) => {
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+  }
+
+  onSubmimtUser(){
+    console.log(this.state)
+  }
 
   render(){
     return(
       <div>
-        <h1>THis is a Inbox Page</h1>
+        <h1>This is a Inbox Page</h1>
+        <div>
+          <label>Enter your Name :</label>
+          <input type="text" placeholder="Please enter your name" 
+          onChange={this.onHandleInput} name="name"/>
+        </div>
+        <div>
+          <label>Enter your Job Role :</label>
+          <input type="text" placeholder="Please enter your job role"
+          onChange={this.onHandleInput} name="job"/>
+        </div>
+        <button onClick={() => this.onSubmimtUser()}>Submit User</button>
       </div>
     )
   }
