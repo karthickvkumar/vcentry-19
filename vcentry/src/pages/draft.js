de0b0860c4c068e748f0596c7897e9da1fdf02c7
kvelraj_concord <kvelraj@concord.net>
2021-10-13 08:40:30 +0530

    delete

--- vcentry/src/pages/draft.js
+++ vcentry/src/pages/draft.js
@@ -1,11 +1,28 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class DraftPage extends Component{
+
+  onDeleteUser(){
+    const url = "https://reqres.in/api/users/2";
+
+    axios.delete(url)
+      .then((response) => {
+        const serverData = response.data;
+        console.log(serverData);
+        alert("Successfully Deleted the User Profile");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Something went worng")
+      })
+  }
 
   render(){
     return(
       <div>
         <h1>THis is a Draft Page</h1>
+        <button onClick={() => this.onDeleteUser()}>Delete User Account</button>
       </div>
     )
   }
